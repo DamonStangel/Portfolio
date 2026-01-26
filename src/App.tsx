dcb1468f78bb8179dbfca9b959262d345f1b3a58
damonjuice <damonstangel@yahoo.com>
2026-01-26 02:32:18 -0600

    Added some more UI styling updates and another fake project tab to the personal projects.

--- src/App.tsx
+++ src/App.tsx
@@ -189,22 +189,21 @@ useEffect(() => {
               <div>
                 <h3>Back End</h3>
                 <ul>
-                  <li>Python</li>
                   <li>Node.js</li>
-                  <li>.NET</li>
-                  <li>C#</li>
                   <li>mySQL</li>
+                  <li>C# / .NET</li>
+                  <li>Python</li>
                 </ul>
               </div>
               <div>
                 <h3>DevOps & Tools</h3>
                 <ul>
-                  <li>OpenSearch API</li>
-                  <li>Lighthouse (DevTools)</li>
+                  <li>GitHub / Git</li>
+                  <li>Lighthouse</li>
                   <li>Docker</li>
-                  <li>Git/GitHub</li>
-                  <li>AWS (Some exp.)</li>
-                  <li>CI/CD (GitHub Actions, Jenkins, etc.)</li>
+                  <li>AWS</li>
+                  <li>CI/CD (GitHub Actions)</li>
+                  <li>OpenSearch API</li>
                 </ul>
               </div>
 
@@ -213,7 +212,6 @@ useEffect(() => {
           </div>
           <div 
             className="langIcons" style={{ filter: isDarkMode ? "invert(1)" : "invert(0)" }}>
-
 
                     <img src="https://cdn.jsdelivr.net/npm/simple-icons@13.21.0/icons/typescript.svg" alt="TS" className="tech-icon"/>
                     {/* <img src="https://cdn.jsdelivr.net/npm/simple-icons@v13/icons/html5.svg" alt="HTML5" className="tech-icon"/> */}
@@ -290,6 +288,28 @@ useEffect(() => {
                       </div>
                     </div>
                       <h3 className="text-xl font-bold mb-2">Project Y</h3>
+                      <p className="text-sm text-gray-600 dark:text-gray-400 mb-4">Scheduled Project</p>
+                      <a href="https://github.com/DamonStangel" target="_blank" className="read-more">
+                          Find More{" "}   
+                          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" data-lucide="github" className="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>
+                      </a>
+                  </div>
+                                    <div className="project-tile">
+                    <div className="project-thumbnail mb-4 " onClick={() => window.open("https://github.com/DamonStangel", "_blank")}>
+                      <img 
+                          src="https://opengraph.githubassets.com/1/" 
+                          alt="Project X" 
+                          className="w-full h-full object-cover"
+                      />
+                      <div className="redirect-icon">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" data-lucide="external-link" className="lucide lucide-external-link">
+                              <path d="M15 3h6v6"></path>
+                              <path d="M10 14 21 3"></path>
+                              <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                          </svg>
+                      </div>
+                    </div>
+                      <h3 className="text-xl font-bold mb-2">Project Z</h3>
                       <p className="text-sm text-gray-600 dark:text-gray-400 mb-4">Scheduled Project</p>
                       <a href="https://github.com/DamonStangel" target="_blank" className="read-more">
                           Find More{" "}   

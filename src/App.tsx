d8f55d3b3717705fae113ebf56998d4198411ae3
damonjuice <damonstangel@yahoo.com>
2026-08-05 19:54:16 -0500

    Add new project links

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ import IMG_0624 from "./assets/IMG_0624.jpg";
 import scatty from "./assets/scatty.jpg";
 import updatedPic from "./assets/Updated Pic.jpg";
 import financeApp from "./assets/financeApp.png";
+import nutritionApp from "./assets/Personal Nutrition Tracker.png";
 
 const images = [suitPic, bsuFlowers, ceremonyPic, DougPic, group10, ChillNightWithTheDogs, couplesPic, group40, oldIcon, famPic, hellcat, IMG_0529, couplesPic2, IMG_0543, IMG_0550, IMG_0624, scatty, updatedPic];
 
@@ -276,8 +277,8 @@ useEffect(() => {
                   <div className="project-tile">
                     <div className="project-thumbnail mb-4 " onClick={() => window.open("https://github.com/DamonStangel", "_blank")}>
                       <img 
-                          src="https://opengraph.githubassets.com/1/" 
-                          alt="Project X" 
+                          src={nutritionApp} 
+                          alt="Project Personal Nutrition Tracker" 
                           className="w-full h-full object-cover"
                       />
                       <div className="redirect-icon">
@@ -288,8 +289,8 @@ useEffect(() => {
                           </svg>
                       </div>
                     </div>
-                      <h3 className="text-xl font-bold mb-2">Project Y</h3>
-                      <p className="text-sm text-gray-600 dark:text-gray-400 mb-4">Scheduled Project</p>
+                      <h3 className="text-xl font-bold mb-2">Project Personal Nutrition Tracker</h3>
+                      <p className="text-sm text-gray-600 dark:text-gray-400 mb-4">Nutrition Application</p>
                       <a href="https://github.com/DamonStangel?tab=repositories" target="_blank" className="read-more">
                           Find More{" "}   
                           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" data-lucide="github" className="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>
